8414d5b9cd718ced0934acc7a5f31065e32e088c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-03-07 14:45:19 +0900

    first import

---
+++ README.md
@@ -0,0 +1,39 @@
+# wandbox-run
+
+use [wandbox](http://melpon.org/wandbox/) as shebang
+
+## Usage
+
+```
+#!/usr/bin/wandbox-run clang-head
+
+#include <stdio.h>
+
+int
+main(int argc, char* argv[]) {
+  puts("hello clang");
+  return 0;
+}
+```
+
+```
+#!/usr/bin/wandbox-run perl-head
+
+use strict;
+use warnings;
+
+warn "hello perl!";
+``
+
+## Requirements
+
+* [json.hpp](https://github.com/nlohmann/json)
+* libcurl
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ wandbox.cxx
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+#include <curl/curl.h>
+
+size_t
+write_cb(char *ptr, size_t size, size_t nmemb, std::string *strm) {
+  size_t len = size * nmemb;
+  strm->append(ptr, len);
+  return len;
+}
+
+int
+main(int argc, char* argv[]) {
+  if (argc < 3) {
+	std::cerr << "usage: " << argv[0] << " [compiler] [file] [arguments...]" << std::endl;
+    return 1;
+  }
+  std::stringstream ss;
+  if (argc == 3) {
+    std::ifstream in(argv[2], std::ifstream::in);
+    std::string line;
+    std::getline(in, line);
+    ss << in.rdbuf();
+  } else {
+    ss << std::cin.rdbuf();
+  }
+
+  nlohmann::json obj;
+  obj["compiler"] = argv[1];
+  obj["code"] = ss.str();
+
+  ss.str("");
+  ss.clear(std::stringstream::goodbit);
+  ss << obj;
+
+  std::string chunk;
+
+  CURLcode ret;
+  CURL *curl = curl_easy_init();
+  if (curl == NULL) {
+	std::cerr << "curl_easy_init() failed" << std::endl;
+	return 1;
+  }
+  std::string data = ss.str();
+
+  struct curl_slist *headerlist = NULL;
+  headerlist = curl_slist_append(headerlist, "Content-Type: application/json");
+
+  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+  curl_easy_setopt(curl, CURLOPT_URL, "http://melpon.org/wandbox/api/compile.json");
+  curl_easy_setopt(curl, CURLOPT_POST, 1);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chunk);
+  ret = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+
+  if (ret != CURLE_OK) {
+	std::cerr << "curl_easy_perform() failed" << std::endl;
+	curl_easy_strerror(ret);
+	return 1;
+  }
+
+  obj = nlohmann::json::parse(chunk);
+  try {
+	std::cout << obj["program_message"].get<std::string>();
+  } catch(std::exception& e) {
+	std::cerr << obj["compiler_message"].get<std::string>() << std::endl;
+  }
+
+  int code;
+  ss.str("");
+  ss.clear(std::stringstream::goodbit);
+  ss << obj["status"];
+  ss >> code;
+  return code;
+}
